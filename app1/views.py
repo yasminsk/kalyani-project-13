4b73c04bb43f760e766c630dc742f644ff70f343
kalyani <gandikotakalyani334@gmail.com>
2023-03-18 22:58:32 +0530

    first commit

---
+++ app1/views.py
@@ -0,0 +1,5 @@
+from django.shortcuts import render
+
+# Create your views here.
+def Mahesh(request):
+    return render(request,'Mahesh.html')
---
+++ app2/urls.py
@@ -0,0 +1,7 @@
+from django.urls import path
+from app2.views import *
+app_name='nothing'
+
+urlpatterns=[
+    path('Pavan/',Pavan,name='Pavan'),
+]
